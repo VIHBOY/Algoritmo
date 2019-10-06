c0990444e4e1598c0b985bf658b10a374185a7d1
Joaquin <Concha.ibaceta.joaquin@gmail.com>
2019-10-06 19:31:01 -0300

    Make

---
+++ lista.h
@@ -0,0 +1,51 @@
+#ifndef _LISTA_H_INCLUDED_
+#define _LISTA_H_INCLUDED_
+
+
+typedef struct{
+	char* color;
+	char* numero;
+	char* tipo;
+	char* nombre;
+}Carta;
+
+typedef struct Element{
+	Carta card;
+	struct Element* next;
+	struct Element* prev;
+}Element;
+
+typedef struct{
+	int index;
+	int size;
+	int jugador;
+	Element* curr;
+	Element* head;
+	Element* tail;
+}LinkedList;
+int Esjugable(Carta* Jugada, Carta* Ultima);
+Carta new_Carta(char* c, char* n, char* t);
+void Carta_creado(Carta card);
+void Carta_print(Carta card) ;
+void Carta_borrado(Carta card);
+Element new_Element();
+void LList_clear(LinkedList* list);
+LinkedList new_LList();
+int Efecto(Carta Ultima);
+void LList_printAll(LinkedList* list);
+Carta* Cartaactual(LinkedList* list);
+void Llist_makedir(LinkedList* list);
+void Llist_deletedir(LinkedList* list);
+int LList_append(LinkedList* list,Carta card);
+void LList_printAll2(LinkedList* list, int* n, LinkedList* Ultima);
+int LList_moveTo(LinkedList* list,int pos);
+int LList_remove(LinkedList* list);
+void LList_reLink(LinkedList* dest,LinkedList* from);
+void repartir(Carta* card, int jugador) ;
+void Robar(LinkedList* Mazo,LinkedList* Jugador);
+void Carta_borrado2(Carta card, int jugador );
+void Llist_deletedir2(LinkedList* list, int njugador);
+int crearCarpeta(const char *dir, const mode_t mode);
+void JugarCarta(LinkedList* Mano,LinkedList* Ultima);
+void Jugar(Carta* card);
+#endif
